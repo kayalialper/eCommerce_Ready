f204ed8a2ff4eb9d1c61a2f9be3ef0f50a5c0c3e
Alper Kayalı <kayalialper@outlook.com>
2023-04-11 17:18:49 +0300

    - New portfolio page added and from now on, the project will start from IndexKYL.

---
+++ AspNetCore_WebAPP_MVC_PROJE/Controllers/AlperKayaliController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_WebAPP_MVC_PROJE.Controllers
+{
+    public class AlperKayaliController : Controller
+    {
+        public IActionResult IndexKYL()
+        {
+            return View();
+        }
+    }
+}
--- AspNetCore_WebAPP_MVC_PROJE/Program.cs
+++ AspNetCore_WebAPP_MVC_PROJE/Program.cs
@@ -38,6 +38,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=AlperKayali}/{action=IndexKYL}/{id?}");
 
 app.Run();
